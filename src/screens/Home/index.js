186fd50b23c8fbd7d5fd03f5ec743ccff6293537
Guanghui Wang <guanghui-wang@foxmail.com>
2020-03-06 15:09:17 +0800

    home page

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,24 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>首页</Text>
-      </View>
-    );
-  }
+export default function Home(props) {
+  const character = {
+    name: 'Derek Wang',
+    home: 'Shanghai',
+    species: 'basketball player',
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>首页</Text>
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => props.navigation.navigate('My', {item: character})}>
+        <Text style={styles.buttonText}>Go to My Screen</Text>
+        <Text style={styles.buttonText}>Who is {character.name}?</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +26,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ebebeb',
+  },
+  text: {
+    color: '#101010',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    backgroundColor: '#222',
+    borderRadius: 5,
+    padding: 10,
+    margin: 20,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });
