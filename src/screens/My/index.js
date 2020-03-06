653c551c063297033b3e8c991231e5a5f89d21a5
Guanghui Wang <guanghui-wang@foxmail.com>
2020-03-06 15:40:24 +0800

    My page

--- src/screens/My/index.js
+++ src/screens/My/index.js
@@ -1,20 +1,52 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
 
-export default class My extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>我的</Text>
-      </View>
-    );
-  }
+export default function My(props) {
+  const {route} = props;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My Screen</Text>
+
+      {route.params?.item && (
+        <View style={styles.card}>
+          <Text style={styles.cardText}>Name: {route.params?.item.name}</Text>
+          <Text style={styles.cardText}>
+            Home Planet: {route.params?.item.home}
+          </Text>
+          <Text style={styles.cardText}>
+            Species: {route.params?.item.species}
+          </Text>
+        </View>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ebebeb',
+  },
+  text: {
+    color: '#101010',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  card: {
+    width: 350,
+    height: 100,
+    borderRadius: 10,
+    backgroundColor: '#101010',
+    margin: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  cardText: {
+    fontSize: 18,
+    color: '#ffd700',
+    marginBottom: 5,
   },
 });
